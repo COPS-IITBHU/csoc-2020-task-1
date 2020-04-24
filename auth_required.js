ca3b121cceda6b908e6a169cb77ba727dbb50f20
Satendra <satendra.raj1241@gmail.com>
2020-04-24 19:10:24 +0530

    Updated code for auth_req and no_auth_req

--- auth_required.js
+++ auth_required.js
@@ -1,14 +1,6 @@
 /***
  * @todo Redirect the user to login page if token is not present.
  */
-$.ajax({
-    headers: {
-        Authorization: 'Token ' + localStorage.getItem('token'),
-    },
-    type: "GET",
-    url: 'https://todo-app-csoc.herokuapp.com/'+"auth/profile/",
-    error:function () {
-        window.location = "../login";
-        displayErrorToast("Please Login First!");
-    }
-});
+if(localStorage.getItem('token')==null){
+    window.location='./login';
+}
--- no_auth_required.js
+++ no_auth_required.js
@@ -1,16 +1,6 @@
 /***
  * @todo Redirect the user to main page if token is present.
  */
-$.ajax({
-    headers: {
-        Authorization: 'Token ' + localStorage.getItem('token'),
-    },
-    type: "GET",
-    url: 'https://todo-app-csoc.herokuapp.com/'+"auth/profile/",
-    success: function (response,textstatus,xhr) {
-        if(xhr.status==200){
-            window.location = "../";
-            displaySuccessToast("Already Logged in!");
-        }
-    }
-});
+if(localStorage.getItem('token')!=null){
+    window.location='../';
+}
