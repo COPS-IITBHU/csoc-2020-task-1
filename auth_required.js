2c518964c2d692b519d0e6bcd7cf3cfcc1cc409e
RinkiyaKeDad <arshsharma461@gmail.com>
2020-04-25 15:52:49 +0530

    Initial Commit

--- auth_required.js
+++ auth_required.js
@@ -1,3 +1,8 @@
 /***
  * @todo Redirect the user to login page if token is not present.
  */
+ const token = localStorage.getItem('token');
+ console.log(token);
+ if(!token){
+    window.location.href = '/login';
+ }
