aa105e298e3e3b40ffbaf72362ce14240fd2fa55
shubhanshu02 <shubhanshu.saxena2002@gmail.com>
2020-04-26 11:24:27 +0530

    All Tasks Done

--- init.js
+++ init.js
@@ -1,7 +1,21 @@
 function getTasks() {
-    /***
-     * @todo Fetch the tasks created by the user and display them in the dom.
-     */
+    $.ajax({
+        headers: {
+            Authorization: 'Token ' + localStorage.getItem('token'),
+        },
+        url: API_BASE_URL + 'todo/',
+        method: 'GET',
+        success: function(data, status, xhr) {
+            document.getElementsByClassName('list-group todo-available-tasks')[0].innerHTML = '<span class="badge badge-primary badge-pill todo-available-tasks-text">Available Tasks</span>'
+            for (let i in data) {
+                additinDOM(data[i].title,data[i].id);
+            }
+        },
+        error: function(xhr, status, err) {
+            if (xhr.status == 0) displayErrorToast('Intenet Connectivity Lost!');
+            else displayErrorToast('Some Error Occurred');
+        }
+    })
 }
 
 $.ajax({
@@ -15,4 +29,5 @@ $.ajax({
         document.getElementById('profile-name').innerHTML = data.name;
         getTasks();
     }
+
 })
--- main.js
+++ main.js
@@ -71,19 +71,76 @@ function register() {
 }
 
 function login() {
-    /***
-     * @todo Complete this function.
-     * @todo 1. Write code for form validation.
-     * @todo 2. Fetch the auth token from backend and login the user.
-     */
+
+    displayInfoToast("Please wait...");
+    const username = document.getElementById('inputUsername').value.trim();
+    const password = document.getElementById('inputPassword').value;
+
+    if (username == "") {
+        displayErrorToast('Invalid Username');
+        return;
+    }
+    const dataForApiRequest = {
+        username: username,
+        password: password
+    }
+    $.ajax({
+        url: API_BASE_URL + 'auth/login/',
+        method: 'POST',
+        data: dataForApiRequest,
+        success: function(data, status, xhr) {
+            localStorage.setItem('token', data.token);
+            window.location.href = '/';
+            displaySuccessToast('Login Successful..');
+        },
+        error: function(xhr, status, err) {
+            if (xhr.status == 400)  displayErrorToast('Invalid Username/Password');
+            else if (xhr.status == 0) displayErrorToast('Internet Connectivity Lost');
+        }
+    })
+}
+
+function additinDOM(title, id) {
+    folder = document.getElementsByClassName("list-group todo-available-tasks")[0];
+    folder.innerHTML += '<li class="list-group-item d-flex justify-content-between align-items-center" id="' + id + '">\n' +
+                    '<input id="input-button-' + id + '" type="text" class="form-control todo-edit-task-input hideme" placeholder="Edit The Task">\n' +
+                    '<div id="done-button-' + id + '"  class="input-group-append hideme">\n' + 
+                    '   <button class="btn btn-outline-secondary todo-update-task" type="button" onclick="updateTask(' + id + ')">Done</button>\n</div>' +
+                    '<div id="task-'+ id + '" class="todo-task">' + title + '</div>\n' + 
+                    '<span id="task-actions-' + id + '">\n' +
+                    '    <button style="margin-right:5px;" type="button" onclick="editTask(' + id + ')"' + 
+                    '        class="btn btn-outline-warning">\n' + 
+                    '        <img src="https://res.cloudinary.com/nishantwrp/image/upload/v1587486663/CSOC/edit.png" width="18px" height="20px">\n' +
+                    '    </button>\n'+
+                    '    <button type="button" class="btn btn-outline-danger" onclick="deleteTask(' + id + ')">\n' +
+                    '        <img src="https://res.cloudinary.com/nishantwrp/image/upload/v1587486661/CSOC/delete.svg" width="18px" height="22px">' +
+                    '   </button>\n</span>\n</li>\n';
 }
 
 function addTask() {
-    /**
-     * @todo Complete this function.
-     * @todo 1. Send the request to add the task to the backend server.
-     * @todo 2. Add the task in the dom.
-     */
+    displayInfoToast("Adding Task...");
+    const task = document.getElementById('add_task').value.trim();
+    const dataForApiRequest = {
+        title: task
+    }
+
+    $.ajax({
+        headers: {
+            Authorization: 'Token ' + localStorage.getItem('token'),
+        },
+        url: API_BASE_URL + 'todo/create/',
+        method: 'POST',
+        data: dataForApiRequest,
+        success: function(data, status, xhr) {
+            displaySuccessToast('Task Successfully Added..');
+            getTasks();
+            document.getElementById('add_task').value ="";
+        },
+        error: function(xhr, status, err) {
+            if (xhr.status == 0) displayErrorToast('Intenet Connectivity Lost!');
+            else displayErrorToast('Some Error Occurred');
+        }
+    })
 }
 
 function editTask(id) {
@@ -94,17 +151,64 @@ function editTask(id) {
 }
 
 function deleteTask(id) {
-    /**
-     * @todo Complete this function.
-     * @todo 1. Send the request to delete the task to the backend server.
-     * @todo 2. Remove the task from the dom.
-     */
+    displayInfoToast("Deleting The Task...");
+    const dataForApiRequest = {
+        id: id
+    }
+    $.ajax({
+        headers: {
+            Authorization: 'Token ' + localStorage.getItem('token'),
+        },
+        url: API_BASE_URL + 'todo/' + id + '/',
+        method: 'DELETE',
+        data: dataForApiRequest,
+        success: function(data, status, xhr) {
+            displaySuccessToast('Task Successfully Deleted..');
+            let x = document.getElementById(id);
+            x.parentElement.removeChild(x);
+        },
+        error: function(xhr, status, err) {
+            if (xhr.status == 0) displayErrorToast('Intenet Connectivity Lost!');
+            else displayErrorToast('Some Error Occurred');
+        }
+    })
+
 }
 
 function updateTask(id) {
-    /**
-     * @todo Complete this function.
-     * @todo 1. Send the request to update the task to the backend server.
-     * @todo 2. Update the task in the dom.
-     */
+    const title = document.getElementById('input-button-' + id).value.trim();
+    if ((title == "") || (title == document.getElementById('task-'+id).innerText)) {
+        document.getElementById('done-button-'+id).classList.add('hideme');
+        document.getElementById('input-button-'+id).classList.add('hideme');
+        document.getElementById('task-'+id).classList.remove('hideme');
+        document.getElementById('task-actions-'+id).classList.remove('hideme');
+        return 0;
+    }
+    displayInfoToast("Updating the Task...");
+    const dataForApiRequest = {
+        title: title
+    }
+
+    $.ajax({
+        headers: {
+            Authorization: 'Token ' + localStorage.getItem('token'),
+        },
+        url: API_BASE_URL + 'todo/' + id + '/',
+        method: 'PATCH',
+        data: dataForApiRequest,
+        success: function(data, status, xhr) {
+            displaySuccessToast('Task Successfully Changed...');
+
+            document.getElementById('task-'+id).innerText = title;
+            document.getElementById('input-button-' + id).value = "";
+            document.getElementById('done-button-'+id).classList.add('hideme');
+            document.getElementById('input-button-'+id).classList.add('hideme');
+            document.getElementById('task-'+id).classList.remove('hideme');
+            document.getElementById('task-actions-'+id).classList.remove('hideme');    
+        },
+        error: function(xhr, status, err) {
+            if (xhr.status == 0) displayErrorToast('Intenet Connectivity Lost!');
+            else displayErrorToast('Some Error Occurred');
+        }
+    })
 }
