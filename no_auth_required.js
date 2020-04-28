2623f38d26bbf989fcb470d0ac45fe2a42a2219a
K-Kumar-01 <59891164+K-Kumar-01@users.noreply.github.com>
2020-04-28 19:56:45 +0530

    Fixed the functionalities

--- no_auth_required.js
+++ no_auth_required.js
@@ -1,3 +1,6 @@
 /***
- * @todo Redirect the user to main page if token is present.
+ *  Redirect the user to main page if token is present.
  */
+if (localStorage.getItem("token")) {
+  window.location.href = "/";
+}
