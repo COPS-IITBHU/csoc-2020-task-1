893aae6c29667074fe071207d2836d4e75ca7df1
Naz-pikachu <rishabh.jain.met19@itbhu.ac.in>
2020-04-29 21:09:51 +0530

    completed auth_required

--- auth_required.js
+++ auth_required.js
@@ -1,3 +1,8 @@
 /***
  * @todo Redirect the user to login page if token is not present.
  */
+const token = window.localStorage.getItem("token");
+console.log(token);
+if (token === null) {
+  window.location = "./login";
+}
