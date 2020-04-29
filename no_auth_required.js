24e5c476f909f01f833aa79bc62f469375e2da44
Naz-pikachu <rishabh.jain.met19@itbhu.ac.in>
2020-04-30 00:03:16 +0530

    some changes

--- no_auth_required.js
+++ no_auth_required.js
@@ -2,7 +2,7 @@
  * @todo Redirect the user to main page if token is present.
  */
 const token = window.localStorage.getItem("token");
-console.log(token);
+//console.log(token);
 if (token != null) {
   window.location = "../";
 }
