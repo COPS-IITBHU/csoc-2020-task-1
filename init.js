13ff413f7bff095783b0ff5f87f032edcfe703b7
Ankit Kumar Sharma <56964985+AnonySharma@users.noreply.github.com>
2020-04-30 21:10:44 +0530

    Update init.js

--- init.js
+++ init.js
@@ -10,6 +10,12 @@ function getTasks() {
         url: API_BASE_URL + 'todo/',
         method: 'GET',
         success: function(data, status, xhr) {
+            // Khali krdo phle
+            if(data.length===0)
+                $('#listOfTasks').replaceWith('<ul id="listOfTasks" class="list-group todo-available-tasks"><span class="badge badge-primary badge-pill todo-available-tasks-text">No Tasks Available</span></ul>');
+            else
+                $('#listOfTasks').replaceWith('<ul id="listOfTasks" class="list-group todo-available-tasks"><span class="badge badge-primary badge-pill todo-available-tasks-text">Available Tasks</span></ul>');
+
             var dummyTasks='<li class="list-group-item d-flex justify-content-between align-items-center"><input id="input-button-!id!" type="text" class="form-control todo-edit-task-input hideme" placeholder="Edit The Task"><div id="done-button-!id!" class="input-group-append hideme"><button class="btn btn-outline-secondary todo-update-task" type="button" onclick="updateTask(!id!)">Done</button></div><div id="task-!id!" class="todo-task">!title!</div><span id="task-actions-!id!"><button style="margin-right:5px;" type="button" onclick="editTask(!id!)" class="btn btn-outline-warning"><img src="https://res.cloudinary.com/nishantwrp/image/upload/v1587486663/CSOC/edit.png" width="18px" height="20px"></button><button type="button" class="btn btn-outline-danger" onclick="deleteTask(!id!)"><img src="https://res.cloudinary.com/nishantwrp/image/upload/v1587486661/CSOC/delete.svg" width="18px" height="22px"></button></span></li>';
             for (let i = 0; i < data.length; i++) {
                 var temp = dummyTasks.replace(/!id!/g,data[i].id);
