d52a49454f18a2171857524e3acb16611bdaaf0e
Ankit Kumar Sharma <56964985+AnonySharma@users.noreply.github.com>
2020-04-30 22:00:05 +0530

    Update no_auth_required.js

--- no_auth_required.js
+++ no_auth_required.js
@@ -2,8 +2,8 @@
  * @todo Redirect the user to main page if token is present.
  */
 
-// if(!(localStorage.getItem('token')))
-// {
-//     console.log('Token nahi hai!');
-//     window.location.href='login/'; 
-// }
+if(localStorage.getItem('token'))
+{
+   console.log('Token hai!');
+   window.location.href='login/';
+}
